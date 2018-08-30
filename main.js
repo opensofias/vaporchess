5f87a532a32f6055070dda50bcce300d52e18f70
sofias <sofias@radikalismus.com>
2018-08-31 01:58:17 +0200

    trying to restore non-breaking version

--- main.js
+++ main.js
@@ -3,36 +3,34 @@
 const num2piece = num => String.fromCodePoint (num + 9812)
 const piece2num = char => char.codePointAt(0) - 9812
 
-const gameMap = new Map ()
-
 const gameArray = []
 
 onload = ()=> d.body.appendChild (elem ({
 	tag: 'table',
-	content: 8 .map (y => elem ({
+	content: 8 .map (row => elem ({
 		tag: 'tr',
-		content: 8 .map (x =>
-			gameMap .set ({x, y}, elem ({
+		content: 8 .map (col =>
+			gameArray [(row * 8) + col] = elem ({
 				tag: 'td',
-				content: startPos(y, x),
+				content: startPos(row, col),
 				attr: { class:
-					y < 2 ? 'player p0' :
-					y > 5 ? 'player p1' :
+					row < 2 ? 'player p0' :
+					row > 5 ? 'player p1' :
 					''
 				},
 				mixin: {
 					onclick: activate,
-					position: (y * 8) + x,
+					position: (row * 8) + col,
 					onmouseenter: showPoss,
 				}
-})))}))}))
+}))}))}))
 
-const startPos = (y, x) => ({
-	0: '♜♞♝♛♚♝♞♜' [x],
+const startPos = (row, col) => ({
+	0: '♜♞♝♛♚♝♞♜' [col],
 	1: '♟',
 	6: '♙',
-	7: '♖♘♗♕♔♗♘♖' [x],
-}) [y] || ''
+	7: '♖♘♗♕♔♗♘♖' [col],
+}) [row] || ''
 
 const activate = ({target}) => {
 	target.classList.toggle ('active')
@@ -40,38 +38,7 @@ const activate = ({target}) => {
 
 const showPoss = ({target}) => {
 	const {position} = target
-	possibilities (target.innerText, num2coords (position))
+	possibilities (target.innerText, )
 }
 
-const num2coords = n => ({
-	x: n % 8,
-	x: Math.floor(n / 8),
-})
-
-const isValid = ({x, y}) => (x >= 0 && x <= 7 && y >= 0 && y <= 7)
-
-const possibilities = (symbol, {x, y}) => {
-	switch (expression) {
-		case '♟':
-		  return [
-				{ x: x - 1, y: y - 1},
-				{ x, y: y - 1},
-				{ x: x + 1, y: y - 1},
-			] .filter (isValid)
-		case '♙':
-		  return [
-				{ x: x - 1, y: y + 1},
-				{ x, y: y + 1},
-				{ x: x + 1, y: y + 1},
-			] .filter (isValid)
-		case '♜': case '♖':
-			break
-		case '♞': case '♘':
-			break
-		case '♝': case '♗':
-			break
-		case '♛': case '♕':
-			break
-		case '♚': case '♔':
-	}
-}
+const possibilities = '♖♘♗♕♔♙'
